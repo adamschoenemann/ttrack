ec49aa4a32235d5be59928254cd4dfde0fa02c08
Adam Schønemann <adamschoenemann@gmail.com>
2019-06-21 10:04:45 +0200

    more formatting business

--- lib/TTrack/TimeUtils.hs
+++ lib/TTrack/TimeUtils.hs
@@ -1,16 +1,19 @@
 module TTrack.TimeUtils
-    ( parseDurationToDiffTime
-    , parseDurationToSeconds
-    , readSeconds) where
+  ( parseDurationToDiffTime
+  , parseDurationToSeconds
+  , readSeconds) where
 
-import           Data.Time
-import           Text.ParserCombinators.Parsec
 import           Control.Applicative hiding (many)
 
+import           Data.Time
+
+import           Text.ParserCombinators.Parsec
+
 -- parse durations
-data TimeUnit = Hours Integer
-              | Minutes Integer
-              | Seconds Integer
+data TimeUnit
+  = Hours Integer
+  | Minutes Integer
+  | Seconds Integer
   deriving (Show, Read, Eq)
 
 timeUnitToSeconds :: TimeUnit -> Integer
@@ -18,8 +21,9 @@ timeUnitToSeconds (Hours x) = x * 60 * 60
 timeUnitToSeconds (Minutes x) = x * 60
 timeUnitToSeconds (Seconds x) = x
 
-timeUnitsToSeconds :: (Maybe TimeUnit, Maybe TimeUnit, Maybe TimeUnit)
-                   -> Integer
+timeUnitsToSeconds
+  :: (Maybe TimeUnit, Maybe TimeUnit, Maybe TimeUnit)
+  -> Integer
 timeUnitsToSeconds (h, m, s) = f h + f m + f s
   where
     f Nothing = 0
@@ -32,8 +36,9 @@ parseMinutes = parseTimeUnit Minutes 'm'
 parseSeconds = parseTimeUnit Seconds 's'
 
 parseTimeUnit :: (Integer -> TimeUnit) -> Char -> CharParser () TimeUnit
-parseTimeUnit constructor end =
-  constructor . (\x -> read x :: Integer) <$> (many digit <* char end)
+parseTimeUnit constructor end = constructor
+  . (\x -> read x :: Integer)
+  <$> (many digit <* char end)
 
 parseDurationToTimeUnits
   :: CharParser () (Maybe TimeUnit, Maybe TimeUnit, Maybe TimeUnit)
@@ -48,14 +53,15 @@ parseDurationToSeconds s = case (parse parseDurationToTimeUnits "none" s) of
   Right x -> Just $ timeUnitsToSeconds x
 
 parseDurationToDiffTime :: String -> Maybe NominalDiffTime
-parseDurationToDiffTime s = (\x -> fromInteger x :: NominalDiffTime)
-  <$> parseDurationToSeconds s
+parseDurationToDiffTime s =
+  (\x -> fromInteger x :: NominalDiffTime) <$> parseDurationToSeconds s
 
 -- Display duration from seconds
 divRemaind :: Integer -> Integer -> (Integer, Integer)
-divRemaind a b = let x = a `div` b
-                     r = a - (x * b)
-                 in (x, r)
+divRemaind a b =
+  let x = a `div` b
+      r = a - (x * b)
+  in (x, r)
 
 hours :: Integer -> (Integer, Integer)
 hours a = a `divRemaind` (60 * 60)
@@ -66,9 +72,11 @@ minutes a = a `divRemaind` 60
 readSeconds :: Integer -> String
 readSeconds s
   | s < 0 = error "readSeconds does not take negative values"
-  | otherwise = let (h, s') = hours s
-                    (m, s'') = minutes s'
-                    format x c = if x > 0
-                                 then (show x) ++ [c]
-                                 else ""
-                in concatMap (\(x, c) -> format x c) $ zip [h, m, s''] "hms"
+  | otherwise =
+    let (h, s') = hours s
+        (m, s'') = minutes s'
+        format x c =
+          if x > 0
+          then (show x) ++ [c]
+          else ""
+    in concatMap (\(x, c) -> format x c) $ zip [h, m, s''] "hms"
